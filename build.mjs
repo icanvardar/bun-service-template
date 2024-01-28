9b74dbec4a4f278d88c7f354a692d1a33215ad11
Can Vardar <ismailcanvardar@gmail.com>
2024-01-28 06:24:50 +0000

    update build script

--- build.mjs
+++ build.mjs
@@ -1,8 +1,21 @@
 import { copyDeclarationFiles } from "./plugins";
 
-await Bun.build({
-  entrypoints: ["./index.ts"],
-  outdir: "./out",
-  minify: true,
-  plugins: [copyDeclarationFiles()]
+const result = await Bun.build({
+    entrypoints: ["./index.ts"],
+    outdir: "./out",
+    plugins: [copyDeclarationFiles()],
+    target: "bun",
+    splitting: true,
+    naming: "files/[dir]/[name]-[hash].[ext]",
+    root: ".",
+    sourcemap: "external"
 });
+
+if (!result.success) {
+    console.error("Build failed");
+    for (const message of result.logs) {
+        console.error(message);
+    }
+} else {
+    console.log(result.outputs[0]);
+}
