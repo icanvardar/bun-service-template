e3ad1fcc4d9c6bc9a2ee753026fbf3335707b72a
Can Vardar <ismailcanvardar@gmail.com>
2024-01-28 21:55:10 +0000

    update foo.spec.ts

--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -4,20 +4,19 @@
 
 import { expect, test, describe } from "bun:test";
 
-// import Server from "@shared/server";
-// import { FooRoute } from "@routes";
+import Server from "@shared/server";
+import { FooRoute } from "@routes";
 import { configs } from "@config";
-// import { StatusCode } from "@constants";
+import { StatusCode } from "@constants";
 
 describe.only("Test FooRoute", () => {
     const basePath = configs.basePath;
-    // const server = Server.getInstance([new FooRoute()]);
+    const server = Server.getInstance([new FooRoute()]);
 
     test("should return success GET /foo", async () => {
-        // const res = await server.app.request(basePath + "/foo");
-        console.log(basePath);
-        // expect(res.status).toEqual(StatusCode.Ok);
-        // expect(await res.text()).toEqual("Hello world!");
-        expect(1).toEqual(1);
+        const res = await server.app.request(basePath + "/foo");
+
+        expect(res.status).toEqual(StatusCode.Ok);
+        expect(await res.text()).toEqual("Hello world!");
     });
 });
