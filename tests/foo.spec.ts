b37df6e9f6e2e65c9e10e8ca4003ffed27835753
Can Vardar <ismailcanvardar@gmail.com>
2024-01-28 21:31:58 +0000

    update test suite

--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -1,3 +1,7 @@
+/* -------------------------------------------------------------------------- */
+/*                             EXAMPLE TEST SUITE                             */
+/* -------------------------------------------------------------------------- */
+
 import { expect, test, describe } from "bun:test";
 
 import Server from "@shared/server";
@@ -5,7 +9,7 @@ import { FooRoute } from "@routes";
 import { StatusCode } from "@constants";
 
 describe.only("Test FooRoute", () => {
-    const server = new Server([new FooRoute()]);
+    const server = Server.getInstance([new FooRoute()]);
 
     test("should return success GET /foo", async () => {
         const res = await server.app.request("/foo");
