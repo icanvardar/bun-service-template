2fadc2bee8655fbf1913cbbe0a8f17b28edd8bb0
Can Vardar <ismailcanvardar@gmail.com>
2024-01-28 16:40:54 +0000

    add ci config to index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import Server from "@shared/server.ts";
 import { FooRoute } from "@routes";
+import { configs } from "@config";
+import logger from "@shared/logger";
 
 const server = new Server([new FooRoute()]);
+
+if (configs.env == "ci") {
+    logger.info("CI successfully completed!");
+    process.exit(1);
+}
 
 export default server.app;
